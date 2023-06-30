e8b1919eb60ef5a6b9f58fa3ad574de4c93368cb
BrunoKauanL <brunokauanlunardon@gmail.com>
2023-06-29 23:59:55 -0300

    Add Bubble::sort implementation

--- DoubleList/Sorters.cpp
+++ DoubleList/Sorters.cpp
@@ -4,7 +4,7 @@
 
 void BubbleSorter::sort(){
     bool swapped;
-    DoubleNode* ptr1=this->list->getFirst();
+    DoubleNode* ptr1;
 
     if (this->list->length() == 0) {
         return;
@@ -12,12 +12,47 @@ void BubbleSorter::sort(){
 
     do {
         swapped = false;
-        
-    } while{true}
+        ptr1 = this->list->getFirst();
+        while (ptr1->next!=nullptr)
+        {
+            if ((ptr1->iPayload)>(ptr1->next->iPayload)) {
+                // swap nodes
+                DoubleNode* ptr2 = ptr1->next;
+                DoubleNode* temp = ptr2->next;
 
+                // if the node is the head node
+                if(ptr1 == this->list->getFirst()){
+                    ptr2->next = ptr1;
+                    ptr1->next = temp;
+                    ptr1->prev = ptr2;
+                    ptr2->prev = nullptr;
+                    if(temp != nullptr){
+                        temp->prev = ptr1;
+                    }
+                    this->list->setFirst(ptr2);
+                } else {
+                    ptr2->next = ptr1;
+                    ptr1->next = temp;
+                    ptr2->prev = ptr1->prev;
+                    ptr1->prev->next = ptr2;
+                    ptr1->prev = ptr2;
+                    if(temp != nullptr){
+                        temp->prev = ptr1;
+                    }
+                }
+                
+                // swap ptr1 and ptr2 for next comparison
+                DoubleNode* tmp = ptr1;
+                ptr1 = ptr2;
+                ptr2 = tmp;
 
+                swapped = true;
+            }
+            ptr1 = ptr1->next;
+        }
+
+    } while(swapped);
 }
-
 
 
 //     void BubbleSort(Node** headRef) 
